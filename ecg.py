d35809752fb30fde38f02f9f90fd6ac395472f21
memerye <lx66@duke.edu>
2019-10-17 10:36:12 -0400

    recognize the abnormal data pairs

--- ecg.py
+++ ecg.py
@@ -1,5 +1,6 @@
 # ecg.py
 import os
+import math
 
 
 def read_data(file_name):
@@ -22,8 +23,10 @@ def convert_data(data_raw):
     """Convert the data from raw string style to the list numbers
 
     The raw data is the list of the strings with mixed time and ecg
-    signals. This function converts the strings to numbers. It can
-    tolerate the empty row inside or at the end of the csv file.
+    signals. This function converts the strings to numbers. It
+    regards the empty row as the end of the csv file. It can also
+    print the corresponding error message when the data contain some
+    non-numeric entries, missing data, or NaN.
 
     Args:
         data_raw (list): the raw file content
@@ -37,13 +40,29 @@ def convert_data(data_raw):
     for row in data_raw:
         time_and_ecg = row.split(',')
         if time_and_ecg != ['']:
-            time = float(time_and_ecg[0])
-            ecg = float(time_and_ecg[1])
-            Time.append(time)
-            ECG.append(ecg)
+            try:
+                assert time_and_ecg[0] != '' and time_and_ecg[1] != ''
+                time = float(time_and_ecg[0])
+                ecg = float(time_and_ecg[1])
+            except AssertionError:
+                print('It has missing data')
+                continue
+            except ValueError:
+                print('It has non numeric data')
+                continue
+            try:
+                assert not math.isnan(time)
+                assert not math.isnan(ecg)
+            except AssertionError:
+                print('It has NaN data')
+                continue
+            else:
+                Time.append(time)
+                ECG.append(ecg)
+    assert len(Time) == len(ECG)
     return Time, ECG
 
 
 if __name__ == "__main__":
-    data_raw = read_data('test_data1.csv')
+    data_raw = read_data('test_data32.csv')
     Time, ECG = convert_data(data_raw)
--- test_ecg.py
+++ test_ecg.py
@@ -3,22 +3,24 @@ import pytest
 
 
 @pytest.mark.parametrize("data_raw, expected1, expected2", [
-    (["2,-3", "", "4.2,2.3"], [2, 4.2], [-3, 2.3]),
+    (["2,-3", "4.2,2.3"], [2, 4.2], [-3, 2.3]),
     (["1.02,-2.345", "2.13,6.203", ""], [1.02, 2.13], [-2.345, 6.203]),
     ([""], [], []),
+    ([",2.3", "1.3,0.6", "1.2,", "1.4,-0.2"], [1.3, 1.4], [0.6, -0.2]),
+    (["NaN,1.2", "-0.2,1.1", "0.3,1.3", "0.4,NaN"], [-0.2, 0.3], [1.1, 1.3]),
+    (["@,1.2", "0.3,v", "0.4,1.4"], [0.4], [1.4]),
+    (["a,0.2", "0.1,-0.5", "NaN,0.5", ",0.2"], [0.1], [-0.5])
 ])
-def test_end_read(data_raw, expected1, expected2):
+def test_convert_data(data_raw, expected1, expected2):
     """Test the function convert_data that converts the data style
-
     Args:
         data_raw (list): the raw file content
-        expected(list): the contents copied from the file
-
+        expected1(list): the expected time
+        expected2(list): the expected ecg signal
     Returns:
         Error if the test fails
         Pass if the test passes
     """
     from ecg import convert_data
     result1, result2 = convert_data(data_raw)
-    assert result1 == expected1
-    assert result2 == expected2
+    assert result1 == expected1 and result2 == expected2
